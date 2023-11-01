c9b03362912d422667c458157ae312b6dad630cc
wiliami <wiliaup97@gmail.com>
2023-10-31 22:37:30 -0400

    codigo adicionado

--- condicionais/script.js
+++ condicionais/script.js
@@ -1,17 +1,16 @@
-// var vel = 78;
+var vel = 78;
+
+// Multa de trânsito
+console.log(`A velocidade  do seu carro é ${vel}km/h`);
+
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+} else {
+    console.log(`Dirija sempre usando cinto de segurança!`);
+}
 
 
-// console.log(`A velocidade  do seu carro é ${vel}km/h`);
-
-// if(vel > 60) {
-//     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
-// } else {
-//     console.log(`Dirija sempre usando cinto de segurança!`);
-// }
-
-
-
-
+// Nacionalidades
 var país = 'Brasil';
 console.log(`Vivendo em ${país}`);
 
