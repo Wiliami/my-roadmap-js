3f84db379819dd5953b3efa46b2acd1c57355b7c
wiliami <wiliaup97@gmail.com>
2023-11-13 22:32:23 -0400

    repetetion e condicion

---
+++ lacos_de_repeticao/lacos_de _repeticao.js
@@ -0,0 +1,22 @@
+// var count = 1;
+
+/* do...while */
+// do {
+//     console.log(`Passo ${count}`);
+//     count++;
+// } while (count <= 6); 
+
+
+/* while */
+// while(c <= 6) {  
+//      console.log('Olá, tudo bem!');
+//      c++;
+// }
+
+console.log('Vai começar...');
+/* for */
+for(var c = 1; c <= 4; c++) {
+    console.log(c);
+}
+
+
