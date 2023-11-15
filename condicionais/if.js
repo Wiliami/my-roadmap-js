f6fb4af1ab1ed955de95d99b99bc5bd51f8ce735
wiliami <wiliaup97@gmail.com>
2023-11-15 16:10:57 -0400

    adicionado exemplos de condição

--- condicionais/if.js
+++ condicionais/if.js
@@ -1,8 +1,32 @@
-console.log('Antes do if');
+// console.log('Antes do if');
 
-if(true) {
+// if(true) {
+//     console.log('Testando o if');
+// }
+
+
+// console.log('Depois do if');
+
+
+
+// Condicionais
+// var operador1 = 1;
+// var operador2 = 1;
+// var result = 2;
+
+// if(operador1 + operador2 == result) {
+//     console.log('Testando o if');
+// } else {
+//     console.log(operador1 + ' + ' + operador2, 'não é igual a:', result);
+// }
+
+
+// Testando a condição
+if(false) {
     console.log('Testando o if');
 }
 
-
-console.log('Depois do if');w
+// Testando o if com uma condição
+// if(1 + 1 == 2) {
+//     console.log('Testando o if');
+// }
