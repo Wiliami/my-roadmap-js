572dbc74a1d23280ac585fe80e0e597f71c3ebf7
wiliami <wiliaup97@gmail.com>
2023-11-16 12:56:52 -0400

    adicionando exercícios

---
+++ exercises/script.js
@@ -0,0 +1,21 @@
+var vel = 78;
+
+// Multa de trânsito
+console.log(`A velocidade  do seu carro é ${vel}km/h`);
+
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+} else {
+    console.log(`Dirija sempre usando cinto de segurança!`);
+}
+
+
+// Nacionalidades
+var país = 'Brasil';
+console.log(`Vivendo em ${país}`);
+
+if(país != 'Brasil') {
+    console.log(`Você é estrangeiro`);
+} else {
+    console.log(`Você é brasileiro`);
+};
