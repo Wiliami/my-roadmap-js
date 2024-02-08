8a3b383ff5535eeea804601f87b94457e6e5ad0c
wiliami <wiliaup97@gmail.com>
2024-02-07 22:42:56 -0400

    update code

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 /* eslint-disable indent */
-// const apiURL = 'https://api.github.com';
+const apiURL = 'https://api.github.com';
 
 
-// function getData(url) {
-//     fetch(url)
-//     .then(response => {
-//         if(!response.ok) {
-//             throw new Error(`Erro na solicitação: ${response.statusText}`);
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .then(error => { 
-//         console.log('Error:', error);
-//     });
+function getData(url) {
+    fetch(url)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(`Erro na solicitação: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .then(error => { 
+        console.log('Error:', error);
+    });
 
-// }
+}
 
-// getData(apiURL);
+getData(apiURL);
 
 /**
  * 
