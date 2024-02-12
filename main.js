fac588ecf9e6a8ef178a24f6232a75aeee53f638
wiliami <wiliaup97@gmail.com>
2024-02-11 22:47:03 -0400

    update code

--- main.js
+++ main.js
@@ -1,40 +1,18 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable indent */
 const apiURL = 'https://api.github.com';
 
+function validationResponse(response) {
+    if(!response.ok) {
+        throw new Error(`Error na solicitação: ${response.statusText}`); 
+    } else {
+        return response.text(); // tipo da resposta;
+    }
+}
 
 function getData(url) {
     fetch(url)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(`Erro na solicitação: ${response.statusText}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .then(error => { 
-        console.log('Error:', error);
-    });
-
+    .then(response => validationResponse(response))
+    .then((data) => console.log(data)) 
+    .catch(error => console.log('Error:', error) );
 }
-
-getData(apiURL);
-
-
-function getDataGithub(url) {
-    fetch(url)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(`Error na solicitação: ${response.statusText}`);
-        }
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log('Error:', error);
-    });
-}
-
-getDataGithub(apiURL);
