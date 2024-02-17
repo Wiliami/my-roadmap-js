c298111d04d0a1e26ce4a3f0572bdbbc5ed1c463
wiliami <wiliaup97@gmail.com>
2024-02-16 20:01:20 -0400

    add call api github profile

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable indent */
-const apiURL = 'https://api.github.com';
+import ServiceGithub from '..js';
 
-function validationResponse(response) {
-    if(!response.ok) {
-        throw new Error(`Error na solicitação: ${response.statusText}`); 
-    } else {
-        return response.text(); // tipo da resposta;
-    }
-}
-
-function getData(url) {
-    fetch(url)
-    .then(response => validationResponse(response))
-    .then((data) => console.log(data)) 
-    .catch(error => console.log('Error:', error) );
-}
+// classe serve de modelo pra criação de objetos
+const serviceGithub = new ServiceGithub();
+serviceGithub.finder('Wiliami');    
