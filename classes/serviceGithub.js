1e1404bf2c40b0636f66a6e940c90f18f60a8fbb
wiliami <wiliaup97@gmail.com>
2024-02-16 22:47:39 -0400

    update code

--- classes/serviceGithub.js
+++ classes/serviceGithub.js
@@ -1,21 +1,38 @@
 /* eslint-disable indent */
 class ServiceGithub {
-    constructor() {
-        this.call();
-    }
+    url = 'https://api.github.com/users';
+    client_id = '9d71a8c4106f4ea74ef9';
+    client_secret = 'f9ff9a14fad8c26a809624c6bd2007690311b4af';
+    profile_response = '';
 
-    call(url) {
-        fetch(url)
-        .then((response) => {
-            if(!response.ok) {
-                throw new Error(`Error na solicitação: ${response.statusText}`);
+    /**
+     * 
+     * @param {string} user Recebe o parâmetro em string
+     * @returns 
+     */
+
+    async finder(user) {
+        try {
+
+            if(typeof user != 'string') {
+                console.log('Parâmetro inválido! A função aceita somente string');
+            } else {
+                this.profile_response = await fetch(
+                    `${this.url}/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+                );
+                if(!this.profile_response.ok) {
+                    throw new Error(`Error na solicitação: ${this.profile_response.status}`);
+                }
+                
+                return await this.profile_response.json();
             }
-            return response.json();
-        })
-        .then(data => console.log(data))
-        .catch(error => console.log('Error:', error));
+
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
 }
 
-const apiGithub = new ServiceGithub();
-apiGithub.call('https://api.github.com');
+
+export default ServiceGithub;
