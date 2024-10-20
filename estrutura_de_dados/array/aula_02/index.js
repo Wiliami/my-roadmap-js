5e37f7f0b5ef9f3238e5589df826d3ad0bc9e6ef
wiliami <wiliaup97@gmail.com>
2024-10-19 22:51:37 -0400

    feat: new feature

--- estrutura_de_dados/array/aula_02/index.js
+++ estrutura_de_dados/array/aula_02/index.js
@@ -1,15 +1,26 @@
-const users = [
-    { name: 'Wiliamis', age: 27 },
-    { name: 'Kipper Dev', age: 28 },
-    { name: 'Filipe Deschamps', age: 38 },
-    { name: 'Justin Bieber', age: 15 },
-];
+// const users = [
+//     { name: 'Wiliamis', age: 27 },
+//     { name: 'Kipper Dev', age: 28 },
+//     { name: 'Filipe Deschamps', age: 38 },
+//     { name: 'Justin Bieber', age: 15 },
+// ];
 
 
-users.forEach(user => {
-    if(user.age < 18) {
-        console.log(user.name);
-    } else {
-        console.log(user);
-    }
-});
+// users.forEach(user => {
+//     console.log(user);
+// });
+
+
+// users.map(user => {
+//     console.log(user);
+// });
+
+
+var cores = ['amarelo', 'verde', 'azul', 'branco', 'vermelho'];
+
+// console.log(cores.length);
+
+
+for(i=0; i <= 4; i++) {
+    console.log(cores[0]);
+}
