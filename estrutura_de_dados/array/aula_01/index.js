29d4648fe061c90538637a3abdf8f0525d5a4ba3
wiliami <wiliaup97@gmail.com>
2024-10-22 23:29:34 -0400

    feat: new feature

--- estrutura_de_dados/array/aula_01/index.js
+++ estrutura_de_dados/array/aula_01/index.js
@@ -31,21 +31,21 @@ const data = [
 
 
 /* Map */
-const users = data.map((user, index) => {
+// const users = data.map((user, index) => {
 
-	if(user.salary <= 2500) {
-		user.salary += 900;
-		user.bonus += 1;
-	};
+// 	if(user.salary <= 2500) {
+// 		user.salary += 900;
+// 		user.bonus += 1;
+// 	};
 
-	return {
-		name: user.name,
-		salary: user.salary,
-		bonus: user.bonus
-	}
+// 	return {
+// 		name: user.name,
+// 		salary: user.salary,
+// 		bonus: user.bonus
+// 	}
 
-});
-console.log(users);
+// });
+// console.log(users);
 
 
 /* Reverse */
