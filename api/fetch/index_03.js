207739fdacbb30931d7fc72a7e161a51f254bf3e
wiliami <wiliaup97@gmail.com>
2024-12-09 17:51:51 -0300

    feat: new feature

---
+++ api/fetch/index_03.js
@@ -0,0 +1,13 @@
+import users from "./index_02"
+
+async function getData() {
+    const resutConvert = await fetch(users)
+    .then(response => response.json())
+    .catch(err => {
+        console.error(err.message)
+    })
+
+    console.log(resutConvert)
+}
+
+getData()
