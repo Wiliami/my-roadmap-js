d5bb9a1382e3c0f772941892a6c1eeb4cec2e210
wiliami <wiliaup97@gmail.com>
2024-12-18 18:09:30 -0300

    feat: new feature

---
+++ estrutura_de_dados/objeto/aula_03.js
@@ -0,0 +1,29 @@
+var pessoa = {
+    nome: ["Bob", "Smith"],
+    idade: 32,
+    sexo: "masculino",
+    interesses: ["música", "esquiar"],
+    bio: function () {
+      alert(
+        this.nome[0] +
+          " " +
+          this.nome[1] +
+          " tem " +
+          this.idade +
+          " anos de idade. Ele gosta de " +
+          this.interesses[0] +
+          " e " +
+          this.interesses[1] +
+          ".",
+      );
+    },
+    saudacao: function () {
+      alert("Oi! Eu sou " + this.nome[0] + ".");
+    },
+  };
+
+  // console.log(pessoa.idade)
+  console.log(pessoa.interesses[0])
+  // console.log(pessoa.bio())
+  
+  console.log(pessoa["sexo"])
