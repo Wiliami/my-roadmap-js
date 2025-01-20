e3dbe83c02f102bc205397b70979f700151a3292
wiliami <wiliaup97@gmail.com>
2025-01-20 18:28:07 -0400

    feat: new feature study

--- classes/WelcomeUser.js
+++ classes/WelcomeUser.js
@@ -10,5 +10,41 @@ class createUser {
     }
 }
 
-const create = new createUser('Wiliamis', 'wiliamis@gmail.com', '123')
-create.execute()
+// const create = new createUser('Wiliamis', 'wiliamis@gmail.com', '123')
+// create.execute()
+
+
+class Retangulo {
+    constructor(altura, largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    get area() {
+        return this.calculaArea();
+    }
+
+    calculaArea() {
+        return this.altura * this.largura;
+    }
+}
+
+
+// const p = new Retangulo(10, 10)
+
+// console.log(p.area)
+
+
+
+
+
+class Exemplo {
+    get propriedade() {
+        return 'Olá, mundo!';
+        // return new String('Olá, mundo')
+    }
+}
+
+
+const exemplo = new Exemplo()
+console.log(exemplo.propriedade)
