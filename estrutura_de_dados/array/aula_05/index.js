7835f2d6c5d5ca4361ce366acd99e419b81370bf
wiliami <wiliaup97@gmail.com>
2025-01-27 18:40:30 -0400

    feat: new feature

---
+++ estrutura_de_dados/array/aula_05/index.js
@@ -0,0 +1,19 @@
+// Veficar se um número é PAR
+
+// const array = [1, 2, 3, 4, 5];
+
+// const result = array.filter(item => item % 2 === 0)
+// console.log(result)
+
+
+
+const array = [1, 2, 3, 4, 5]
+
+const result = array.filter(item => item % 2 === 0)
+// console.log(result)
+
+
+function sum(...numbers) {
+    return numbers.reduce((a, b) => a + b) 
+}
+console.log(sum(10, 20))
