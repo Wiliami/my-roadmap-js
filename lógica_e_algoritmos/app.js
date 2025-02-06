5c6508ba5a4a4609764780e9a0a505faa37f32b6
wiliami <wiliaup97@gmail.com>
2025-02-06 17:26:18 -0400

    feat: new feat

---
+++ lógica_e_algoritmos/app.js
@@ -0,0 +1,31 @@
+// const inputNumero = document.getElementById('inputNumero');
+// const btnCalcular = document.getElementById('btnCalcular');
+// const outTabuada = document.getElementById('outTabuada');
+
+// function mostraTabuada() {
+
+//     let numero = Number(inputNumero.value);
+//     let tabuada = '';
+
+//     // validar o input do usuário
+//     if(inputNumero.value == '' || isNaN(numero)) {
+//         alert('Digite um número positivo!');
+//         inputNumero.value = '';
+//         inputNumero.focus();
+//         return;
+//     }
+
+//     for(let index = 1; index <= 10; index++) {
+//         tabuada += `${numero} x ${index} = ${numero * index}\n`;
+//     }
+
+//     outTabuada.textContent = `${tabuada}`;
+// }
+
+// btnCalcular.addEventListener('click', mostraTabuada);
+
+
+
+
+const myNumber = Number('1223')
+console.log(myNumber)
