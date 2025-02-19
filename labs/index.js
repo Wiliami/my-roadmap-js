6789560a5517c91165088a3a5d169c0e8b9c0f69
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2025-02-19 21:27:11 +0000

    learn: feat function decorator

---
+++ labs/index.js
@@ -0,0 +1,16 @@
+// console.log("Hello world");
+
+/* Testando a feat decorator JS*/
+function log() {
+  return "Hello World";
+}
+
+class Hello {
+  // @log
+  greet() {
+    return "Hello world.";
+  }
+}
+
+const teste = new Hello();
+console.log(teste.greet());
