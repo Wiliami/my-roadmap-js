7b027964b2256a8a2b6b66447c961ccb72af9572
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2025-03-06 06:24:06 +0000

    feat: update repo

---
+++ api/fetch/data.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: 1,
+    name: "Arthur Silva",
+    email: "arthur.sivla@gmail.com",
+    age: 34,
+    gender: "male",
+  },
+  {
+    id: 2,
+    name: "Mateus Biassi",
+    email: "mateus.31@gmail.com",
+    age: 19,
+    gender: "male",
+  },
+  {
+    id: 3,
+    name: "Karolyne Santos",
+    email: "karolyne@gmail.com",
+    age: 18,
+    gender: "female",
+  },
+];
+
+export default users;
