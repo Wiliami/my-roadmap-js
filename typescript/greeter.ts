b47a6d42f1e2bf58602d9accbb7a283d5a0fb546
wiliami <wiliaup97@gmail.com>
2025-03-20 16:42:17 -0400

    feat: learning types static semantic

---
+++ typescript/greeter.ts
@@ -0,0 +1,28 @@
+type User = {
+    name: string
+    age: number
+}
+
+function isAdult(user: User): boolean {
+    return user.age >= 18;
+}
+
+const justine = { name: 'Justine', age: 23 }
+
+const isJustineIsAnAdult = isAdult(justine)
+// console.log(isJustineIsAnAdult)
+
+
+function showUserScreen(user: string): string {
+    return "Hello " + user
+}
+
+console.log(showUserScreen('Wiliamis'))
+
+
+// function greeter(person: string) {
+//     return "Hello " + person
+// }
+
+// let user = [1, 2, 4]
+// console.log(greeter(user))
