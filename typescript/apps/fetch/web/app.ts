e665369140efd216635e59a86a6b4c04bedf6ef0
wiliami <wiliaup97@gmail.com>
2025-03-21 00:09:02 -0400

    feat: new feature

--- typescript/apps/fetch/web/app.ts
+++ typescript/apps/fetch/web/app.ts
@@ -1,6 +1,4 @@
-const url = 'https://jsonplaceholder.typicode.com/users'
-
-
-fetch(url)
+fetch('https://jsonplaceholder.typicode.com/users')
 .then(response => console.log(response.json()))
 .then(data => console.log(data))
+.then(err => console.log('Erro ao recuper dados da API:', err))
