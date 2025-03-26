9d43ae9cc760574f34421810d621f76183dd84bd
wiliami <wiliaup97@gmail.com>
2025-03-25 23:10:28 -0400

    feat: new feature

---
+++ estrutura_de_dados/array/aula_06/app.ts
@@ -0,0 +1,7 @@
+const numbers: number[] = [1, 2, '3', 4, 5]
+const fruits: string[] = ['maça', 'banana']
+const question: boolean[] = [false]
+
+console.log(question)
+console.log(numbers)
+console.log(fruits)
