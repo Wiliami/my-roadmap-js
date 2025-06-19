f3f738d10fb42d62a09b918f1671f70fd8776b85
wiliami <wiliaup97@gmail.com>
2025-06-18 22:03:28 -0400

    feat: update

---
+++ variaveis/aula_02.js
@@ -0,0 +1,75 @@
+// function dizerMeuNome() { // Escopo Pai
+//     var a = "a";
+
+//     return function encontraNome() { // Escopo Filho
+//         var b = "b";
+//         console.log(a);
+
+
+//         return function mostrarNome() {
+//             var c = "c";
+//             console.log(b);
+
+
+//             return "Wiliamis Oliveira";
+//         }
+//     }
+
+// }
+
+// console.log(dizerMeuNome()()());
+
+// console.log(nome)
+
+// var nome = "Wiliamis";
+// nome = "Dudu";
+// console.log(nome)
+
+// function mostrarNome() {
+//     return nome;
+// }
+
+// console.log(mostrarNome())
+
+
+
+// let nome;
+
+// if(!nome) {
+//     console.log("O nome ainda não foi definido.")
+// }
+
+// console.log(nome = "Wiliamis");
+
+
+// function saudacao(hora) {
+//     let saudacao;
+
+
+//     if(hora < 12) {
+//         saudacao = "Bom dia!";
+//     } else if(hora < 18) {
+//         saudacao = "Boa tarde!";
+//     } else {
+//         saudacao = "Boa noite!";
+//     }
+
+
+//     console.log(saudacao);
+// }
+
+
+// saudacao(0)
+
+
+// console.log(nome)
+
+
+// let nome = "Wiliamis"
+
+testExpression()
+
+var testExpression = function() {
+    console.log('Testando Function Expressions');
+}
+
