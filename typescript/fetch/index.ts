8cf0b2134110347c8b957d0d519945c6bf93c7c2
wiliami <wiliaup97@gmail.com>
2025-06-26 17:24:03 -0400

    feat: new feature

--- typescript/fetch/index.ts
+++ typescript/fetch/index.ts
@@ -5,7 +5,7 @@ interface User {
     public_repos: number,
 }
 
-async function getProfileUserGithub(): Promise<User> {
+async function getProfileUserGithub(): Promise<User | undefined> {
     const response = await fetch('https://api.github.com/users/Wiliami')
 
     if(response.ok) {
@@ -21,6 +21,40 @@ async function getProfileUserGithub(): Promise<User> {
         throw new Error('Erro ao buscar dados de perfil de usuário.');
     }
 }
+ interface Letter {
+    result: string
+ }
 
+async function getLetras(): Promise<Letter> {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
 
+    const arr = ['a', 'b', 'c', 'd', 'e', 'f']
+
+    const result = arr.map(letter => {
+        console.log(letter)
+    })
+
+    return result
+}
+
+async function getUsers() {
+    // delay 1 sec
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+    return [
+        { name: "Wiliamis", age: 25 },
+        { name: "Bob", age: 32 },
+        { name: "Charlie", age: 28 },
+        { name: "Diana", age: 30 },
+        { name: "Ethan", age: 22 },
+        { name: "Fiona", age: 27 },
+        { name: "George", age: 35 },
+        { name: "Hannah", age: 29 },
+        { name: "Ian", age: 19 },
+        { name: "Julia", age: 17 }
+    ];
+}
+
+console.log(getLetras())
+console.log(getUsers())
 console.log(getProfileUserGithub())
