492eeb202985d5ef2c7742bdb85695d44ff63253
wiliami <wiliaup97@gmail.com>
2025-07-03 17:17:12 -0400

    feat: new feature

--- typescript/begginer/exercicio_07.ts
+++ typescript/begginer/exercicio_07.ts
@@ -1,1 +1,45 @@
-fetch('t')
+// function identity(arg: number): number {
+//     return arg
+// }
+
+// function identity(arg: any): any {
+//     return arg
+// }
+
+function identity<Type>(arg: Type): Type {
+    return arg
+}
+
+// console.log(identity({
+//     name: 'Wiliamis',
+//     age: 27,
+// }))
+
+
+console.log(identity(22))
+
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({
+            name: 'Wiliamis',
+            age: 27
+        })
+    }, 3000)
+})
+
+
+async function executePromise() {
+    try {
+        const response = await myPromise   
+
+        return response
+
+    } catch (err) {
+        return new Error('Deu ruim', err)
+    }
+}
+
+
+
+console.log(executePromise())
