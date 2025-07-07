e28545319cbcdbcbf3bd48dbafed33110f31e730
wiliami <wiliaup97@gmail.com>
2025-07-07 08:17:34 -0400

    feat: new feature

--- typescript/begginer/exercicio_07.ts
+++ typescript/begginer/exercicio_07.ts
@@ -18,28 +18,58 @@ function identity<Type>(arg: Type): Type {
 
 console.log(identity(22))
 
-
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve({
-            name: 'Wiliamis',
-            age: 27
-        })
-    }, 3000)
-})
-
-
-async function executePromise() {
-    try {
-        const response = await myPromise   
-
-        return response
-
-    } catch (err) {
-        return new Error('Deu ruim', err)
-    }
-}
+// await new Promise((resolve) => setTimeout(resolve, 1000))
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve({
+//             name: 'Wiliamis',
+//             age: 27
+//         })
+//     }, 3000)
+// })
 
 
+// async function executePromise() {
+//     try {
+//         const response = await myPromise   
 
-console.log(executePromise())
+//         return response
+
+//     } catch (err) {
+//         return new Error('Deu ruim', err)
+//     }
+// }
+
+
+
+// console.log(executePromise())
+
+
+
+// import { useState, useEffect } from 'react'
+
+// type Users = {
+//     id: number
+//     name: string
+//     email: string
+// }
+
+// export default function Users() {
+//     const [ users, setUsers ] = useState<Users>([])
+//     const [ loading, setLoading ] = useState([])
+
+//     useEffect(() => {
+//         async function getUsers() {
+//             const response = await fetch('http://localhost:3000/users')
+       
+//             if(response.ok) {
+//                 return await response.json()
+//             } else {
+//                 throw new Error('Erro ao buscar dados.')
+//             }
+//         }
+
+//         getUsers()
+
+//     }, [])
+// }
