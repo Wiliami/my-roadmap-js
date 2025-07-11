8052830941174866a1bbb2cc5ce31034760023d6
wiliami <wiliaup97@gmail.com>
2025-07-11 14:07:07 -0400

    feat: new feature

--- typescript/exercicios/aula_11.ts
+++ typescript/exercicios/aula_11.ts
@@ -1,20 +1,36 @@
-type repositoriesData = {
-
+type githubRepos = {
+    id: number,
+    node_id: string,
+    name: string,
+    full_name: string,
 }
 
-async function getReposGithub(): Promise<repositoriesData> {
-    const response = await fetch('https://api.github.com/users/Wiliami')
+async function getGithubRepositories(): Przomise<githubRepos[] | > {
+    const response = await fetch('https://api.github.com/users/Wiliami/repos')
 
-    if(!response.ok) {
-        throw new Error(`Erro ao buscar dados: ${response.statusText}`)
-    }
+    if(!response.ok) throw new Error(`Erro ao buscar dados: ${response.statusText}`)
 
-    const { login, id, bio } = await response.json()
-
-    return {
-
-    }
-
+    const repos = await response.json()
     
-
+    return repos.map(({ id, node_id, name, full_name }) => {
+        return {
+            id,
+            node_id,
+            name,
+            full_name
+        }
+    })
 }
+
+async function fetchRepos() {
+    try {
+        const repos = await getGithubRepositories()
+
+        console.log(repos)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+fetchRepos()
