e5289d575319b7b170119d7e091f4209d054bc3b
wiliami <wiliaup97@gmail.com>
2026-03-25 23:05:03 -0400

    new code

---
+++ primitivos/index.js
@@ -0,0 +1,9 @@
+const username = "Wiliamis"; // string (Tipo primitivo) ❌ não é objeto
+
+username.length
+// const username = new String("Wiliamis") -> username.length
+
+
+// No javascript nem tudo são objetos, tipos primitivos se comportam como objetos
+
+// Tipos primitivos e objetos
---
+++ primitivos/index.md
@@ -0,0 +1,12 @@
+# Tipos primitivos em JS
+1. undefinded
+2. null
+3. number
+4. string
+
+
+### Todos os valores são tipos primitivos ou objetos
+
+# const name = "Wiliamis";
+
+console.log(name.length); // 8
